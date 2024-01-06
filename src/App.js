788378e25662112ec08d163a82ed2e6b9e2da757
mohdsajidshaikhcoder <155801059+mohdsajidshaikhcoder@users.noreply.github.com>
2024-01-06 12:06:34 +0530

    yes

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import {Button} from 'react-bootstrap'
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+
+import Headers from './Components/Hearders'
+
+import Home from './Components/Home';
+import CricketTeam from './Components/CricketTeam';
+import ContactUs from './Components/ContactUs';
+
+
+
+function App() {
+  return (
+    <Router>
+        <div>
+          <Headers />
+          <div className='container mt-3'>
+            <Routes>
+              <Route path='/' Component={CricketTeam}/>
+              <Route path='/cricket-team' Component={CricketTeam}/>
+              <Route path='/join-us' Component={ContactUs}/>
+            </Routes>
+          </div>
+        </div>
+    </Router>
+  );
+}
+
+export default App;
